7593631166a7fb1b59477387f736de4f169ac727
Bhavesh Hirani <bhavesh.hirani@cki.com.au>
2022-05-26 20:07:46 +1000

    code format working copy for startrack

--- src/PostageProduct.php
+++ src/PostageProduct.php
@@ -1,39 +1,41 @@
 <?php
-
-namespace Zoet\Startrack;
-
-/**
- * Read-only Postage Product details
- *
- * @package Zoet\Startrack
- * @author Josh Marshall <josh@jmarshall.com.au>
- *
- * @property string $type
- * @property string[] $lines
- * @property string $suburb
- * @property string $state
- * @property string $postcode
- * @property string $country
- */
-class PostageProduct {
-
-    public $type;
-    public $group;
-    public $product_id;
-    public $contract;
-    public $authority_to_leave_threshold;
-    public $features;
-    public $options;
-    public $shipment_features;
-    public $raw_details = [];
-
-    public function __construct($details) {
-        $this->raw_details = $details;
-        foreach ($details as $key => $data) {
-            if (!property_exists($this, $key)) {
-                continue;
-            }
-            $this->$key = $data;
-        }
-    }
-}
+	
+	namespace Zoet\Startrack;
+	
+	/**
+	 * Read-only Postage Product details
+	 *
+	 * @package Zoet\Startrack
+	 * @author Josh Marshall <josh@jmarshall.com.au>
+	 *
+	 * @property string $type
+	 * @property string[] $lines
+	 * @property string $suburb
+	 * @property string $state
+	 * @property string $postcode
+	 * @property string $country
+	 */
+	class PostageProduct
+	{
+		
+		public $type;
+		public $group;
+		public $product_id;
+		public $contract;
+		public $authority_to_leave_threshold;
+		public $features;
+		public $options;
+		public $shipment_features;
+		public $raw_details = [];
+		
+		public function __construct($details)
+		{
+			$this->raw_details = $details;
+			foreach ($details as $key => $data) {
+				if (!property_exists($this, $key)) {
+					continue;
+				}
+				$this->$key = $data;
+			}
+		}
+	}
