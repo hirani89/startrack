5c3e841d2b53417da933dab8ba4b74129154f5d3
Bhavesh Hirani <bhavesh.hirani@cki.com.au>
2022-05-26 12:29:38 +1000

    initial comit

---
+++ README.md
@@ -0,0 +1,111 @@
+# Australia Post API
+
+Interact with the AusPost API
+
+## Installation
+
+Installation is very easy with composer:
+
+    composer require zoet/startrack
+
+## Setup
+
+Get a business account at Australia Post and request API access
+
+## Usage
+
+```
+<?php
+	$startrack = new \Zoet\Startrack\Startrack('Your API Key', 'Your API Password', 'Your Account Number', $testmode);
+
+	// Get your account details
+	$account = $startrack->getAccountDetails();
+
+	// Create a shipment
+
+	$shipment = $startrack->newShipment()
+		->setFrom(new \Zoet\Startrack\Address([
+			'first_name' => 'Joe',
+			'last_name' => 'Tester',
+			// OR 'name' => 'Joe Tester',
+			'lines' => [
+				'11 MyStreetname Court',
+			],
+			'suburb' => 'MySuburb',
+			'state' => 'QLD',
+			'postcode' => '4503',
+			'country' => 'AU',
+		]))
+		->setTo(new \Zoet\Startrack\Address([
+			'first_name' => 'Mary',
+			'last_name' => 'Tester',
+			'lines' => [
+				'10 ReceiverStreetname St',
+			],
+			'suburb' => 'HerSuburb',
+			'state' => 'NSW',
+			'postcode' => '2430',
+			'country' => 'AU',
+			'phone' => '035555XXXX',
+			'email' => 'mary@XXXXX.com.au',
+		]))
+		->addParcel(new \Zoet\Startrack\Parcel([
+			'item_reference' => 'pkg1',
+			'length' => 5,
+			'height' => 4,
+			'width' => 45,
+			'weight' => 0.55,
+			'value' => 200,
+		]))
+		->addParcel(new \Zoet\Startrack\Parcel([
+			'item_reference' => 'pkg2',
+			'length' => 12,
+			'height' => 12,
+			'width' => 20,
+			'weight' => 1.55,
+			'value' => 50,
+		]));
+
+	// Get costing for the shipment for the various AusPost products available
+	$itemQuotes = $shipment->getQuotes();
+
+	foreach ($itemQuotes as $quote) {
+		var_dump($quote->product_id);
+		var_dump($quote->product_type);
+		var_dump($quote->price_inc_gst);
+	}
+
+	// Set details about the shipment and lodge it
+	$shipment->shipment_reference = 'OurInternalID';
+	$shipment->customer_reference_1 = 'INV #12345';
+	$shipment->customer_reference_2 = '';
+	$shipment->product_id = '7E55'; // The AusPost product returned in the Quote
+	$shipment->delivery_instructions = 'Leave in a dry place out of the sun';
+
+	$shipment->lodgeShipment();
+
+	var_dump($shipment->shipment_id);
+
+	// Print the labels for all the parcels in a number of shipments
+	$shipment_ids = [
+		'NIUK0Eavr1EAAAF0BfAdXIsx',
+		'i9MK0Eav1ywAAAF0t8kdXI84',
+		$shipment->shipment_id,
+	];
+	$label_pdf = $startrack->getLabels($shipment_ids, new \Zoet\Startrack\LabelType([
+		'layout_type' => \Zoet\Startrack\LabelType::LAYOUT_A4_FOUR_PER_PAGE,
+		'branded' => false,
+	]));
+
+	var_dump($label_pdf); // It's the url to the pdf
+
+	// Create a manifest for the shipments
+	$order = $startrack->createOrder([
+		'NIUK0Eavr1EAAAF0BfAdXIsx',
+		'i9MK0Eav1ywAAAF0t8kdXI84',
+	]);
+
+	// Output the manifest pdf to the screen
+	header('Content-Type: application/pdf');
+	die($order->manifest_pdf);
+```
---
+++ src/Shipment.php
@@ -0,0 +1,221 @@
+<?php
+
+namespace Zoet\Startrack;
+
+/**
+ * A shipment, made up of one or more parcels
+ *
+ * @package Zoet\Startrack
+ * @author Josh Marshall <josh@jmarshall.com.au>
+ *
+ * @property Startrack $_startrack
+ * @property string $shipment_reference
+ * @property string $customer_reference_1
+ * @property string $customer_reference_2
+ * @property bool $email_tracking_enabled
+ * @property Address $to
+ * @property Address $from
+ * @property Parcel[] $parcels
+ * @property string $product_id The AusPost product to use for this shipment
+ * @property string $shipment_id The AusPost generated id when lodged
+ * @property \DateTime $shipment_lodged_at The time the shipment was lodged
+ */
+class Shipment {
+
+    private $_startrack;
+    public $shipment_reference;
+    public $customer_reference_1 = '';
+    public $customer_reference_2 = '';
+    public $email_tracking_enabled = true;
+    public $movement_type = 'DESPATCH';
+    public $from;
+    public $to;
+    public $parcels = [];
+    public $delivery_instructions = '';
+
+    public $product_id;
+    public $shipment_id;
+    public $shipment_lodged_at;
+
+    public function __construct($api) {
+        $this->_startrack = $api;
+    }
+
+    /**
+     * Add the To address
+     * @param Address $data The address to deliver to
+     * @return $this
+     */
+    public function setTo($data) {
+        $this->to = $data;
+        return $this;
+    }
+
+    /**
+     * Add the From address
+     * @param Address $data The address to send from
+     * @return $this
+     */
+    public function setFrom($data) {
+        $this->from = $data;
+        return $this;
+    }
+
+    /**
+     * Set the movement type
+     * @param string $data 'DESPATCH' or 'RETURN' or 'TRANSFER'
+     * @return $this
+     */
+    public function setMovementType($data) {
+        $this->movement_type = $data;
+        return $this;
+    }
+
+    public function addParcel($data) {
+        $this->parcels[] = $data;
+        return $this;
+    }
+
+    /**
+     *
+     * @return \Zoet\Startrack\Quote[]
+     * @throws \Exception
+     */
+    public function getQuotes() {
+        $request = [
+            'from' => [
+	              'suburb' => $this->from->suburb,
+                'postcode' => $this->from->postcode,
+                'country' => $this->from->country,
+            ],
+            'to' => [
+	              'suburb' => $this->to->suburb,
+                'postcode' => $this->to->postcode,
+                'country' => $this->to->country,
+            ],
+            'items' => [],
+        ];
+        foreach ($this->parcels as $parcel) {
+            $item = [
+                'item_reference' => $parcel->item_reference,
+                'length' => $parcel->length,
+                'height' => $parcel->height,
+                'width' => $parcel->width,
+                'weight' => $parcel->weight,
+            ];
+            if ($parcel->value) {
+                $item['features'] = [
+                    'TRANSIT_COVER' => [
+                        'attributes' => [
+                            'cover_amount' => $parcel->value,
+                        ]
+                    ]
+                ];
+            }
+            $request['items'][] = $item;
+        }
+        return $this->_startrack->getQuotes($request);
+    }
+
+    public function lodgeShipment() {
+        // Determine if Domestic or International
+        $domestic_shipping = $this->to->country == 'AU';
+
+        if ($domestic_shipping) {
+            // Lodge domestic shipment
+        } else {
+            // Lodge international shipment
+        }
+        $request = [
+            'shipment_reference' => $this->shipment_reference,
+            'customer_reference_1' => $this->customer_reference_1,
+            'customer_reference_2' => $this->customer_reference_2,
+            'email_tracking_enabled' => $this->email_tracking_enabled,
+            'movement_type' => $this->movement_type,
+            'from' => [
+                'name'          => $this->from->name,
+                'business_name' => $this->from->business_name,
+                'lines'         => $this->from->lines,
+                'suburb'        => $this->from->suburb,
+                'state'         => $this->from->state,
+                'postcode'      => $this->from->postcode,
+                'country'       => $this->from->country,
+                'phone'         => $this->from->phone,
+                'email'         => $this->from->email,
+            ],
+            'to' => [
+                'name'          => $this->to->name,
+                'business_name' => $this->to->business_name,
+                'lines'         => $this->to->lines,
+                'suburb'        => $this->to->suburb,
+                'state'         => $this->to->state,
+                'postcode'      => $this->to->postcode,
+                'country'       => $this->to->country,
+                'phone'         => $this->to->phone,
+                'email'         => $this->to->email,
+                'delivery_instructions' => $this->delivery_instructions,
+            ],
+            'items' => [],
+        ];
+        foreach ($this->parcels as $parcel) {
+            $item = [
+                'item_reference' => $parcel->item_reference,
+                'product_id'     => $this->product_id,
+                'length'         => $parcel->length,
+                'height'         => $parcel->height,
+                'width'          => $parcel->width,
+                'weight'         => $parcel->weight,
+                'contains_dangerous_goods' => $parcel->contains_dangerous_goods,
+                'authority_to_leave' => $parcel->authority_to_leave,
+                'safe_drop_enabled' => $parcel->safe_drop_enabled,
+                'allow_partial_delivery' => $parcel->allow_partial_delivery,
+            ];
+            if ($parcel->value) {
+                $item['features'] = [
+                    'TRANSIT_COVER' => [
+                        'attributes' => [
+                            'cover_amount' => $parcel->value,
+                        ]
+                    ]
+                ];
+            }
+            $request['items'][] = $item;
+        }
+
+        $response = $this->_startrack->shipments(['shipments' => $request]);
+
+        foreach ($response['shipments'] as $shipment) {
+            $this->shipment_id = $shipment['shipment_id'];
+            $this->shipment_lodged_at = new \DateTime($shipment['shipment_creation_date']);
+            foreach ($shipment['items'] as $item) {
+                foreach ($this->parcels as $key => $parcel) {
+                    if ($parcel->item_reference != $item['item_reference']) {
+                        continue;
+                    }
+                    $this->parcels[$key]->item_id = $item['item_id'];
+                    $this->parcels[$key]->tracking_article_id = $item['tracking_details']['article_id'];
+                    $this->parcels[$key]->tracking_consignment_id = $item['tracking_details']['consignment_id'];
+                }
+            }
+        }
+    }
+
+    /**
+     * Get the labels for this shipment
+     * @param LabelType $labelType
+     * @return string url to label
+     * @throws \Exception
+     */
+    public function getLabel($labelType) {
+        return $this->_startrack->getLabels([$this->shipment_id], $labelType);
+    }
+
+    /**
+     * Delete this shipment
+     * @throws \Exception
+     */
+    public function deleteShipment() {
+        return $this->_startrack->deleteShipment($this->shipment_id);
+    }
+
+}
